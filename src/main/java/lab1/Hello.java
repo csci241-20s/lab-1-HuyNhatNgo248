b804912adb799399b9d9bc0c48ed38c8e70a0532
Huy Nhat Ngo <ngoh@wwu.edu>
2020-04-11 13:12:52 -0700

    edit exclaimation mark to fit with lab format

--- src/main/java/lab1/Hello.java
+++ src/main/java/lab1/Hello.java
@@ -1,13 +1,15 @@
 package lab1;
 
 public class Hello {
-  public static void main(String[] args) {
-    String s = "Hello, ";
-    for (int i = 0; i < args.length; i++) {
-      s += args[i] + " ";
-    }
+    public static void main(String[] args) {
+        String s = "Hello, ";
+        for (int i = 0; i < args.length; i++) {
+            if (i < args.length - 1)
+                s += args[i] + " ";
+            else
+                s += args[i] + "!"
+        }
 
-    s += "!"; 
-    System.out.println(s);
-  }
+        System.out.println(s);
+    }
 }
